29dcc5626c1449553e5047864671f93ec2b26a3b
Oleg <pakolegdev@gmail.com>
2022-12-26 17:00:38 +0300

    code refactoring

--- api/src/main/java/com/epam/esm/config/security/WebSecurityConfig.java
+++ api/src/main/java/com/epam/esm/config/security/WebSecurityConfig.java
@@ -28,7 +28,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Override
-
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
                 .csrf().disable()
